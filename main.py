0f4f0d474b232f48550909b637b35e8b087b1945
Aravind <rn.aravind05@gmail.com>
2025-03-27 11:15:54 +0530

    boundary input

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import networkx as nx
+import numpy as np
+import matplotlib.path as mpath
 
-def graph_input():
+'''def graph_input():
     G=nx.Graph()
     n=int(input("Enter no. of nodes:"))
     for i in range(n):
@@ -12,8 +14,36 @@ def graph_input():
         G.add_edge(a,b) 
     return G
 
-G=graph_input()
-is_planar = nx.is_planar(G)
-print(is_planar)
+
+
+g=graph_input()'''
+#is_planar = nx.is_planar(G)
+
 #print(G.nodes)
 #print(G.edges)
+
+def boundary_input():
+    print("Enter boundary vertices (space separated node pairs eg. 1 2), and after the last vertex enter the coords of the first vertex(to close the boundary).")
+    boundary=[]
+    a,b=map(int,input().split())
+    boundary.append((a,b))
+    max_x,max_y=a,b
+    while True:
+        a,b=map(int,input().split())
+        boundary.append((a,b))
+        if a>max_x:
+            max_x=a
+        if b>max_y:
+            max_y=b
+        if a==boundary[0][0] and b==boundary[0][1]:
+            break
+    bpath=mpath.Path(boundary)
+    matrix=np.zeros((max_y,max_x))
+    for i in range(0,max_y):
+        for j in range(0,max_x):
+            if bpath.contains_point((j+0.5,i+0.5)):
+                matrix[i][j]=1  
+    return matrix
+
+m = boundary_input()
+
